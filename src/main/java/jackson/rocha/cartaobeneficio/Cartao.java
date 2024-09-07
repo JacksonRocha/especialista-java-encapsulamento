6c7db99f0039c4931d527d064d4456d198cadff6
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-07 17:12:13 -0300

    Encapsulamento criando classes

---
+++ src/main/java/jackson/rocha/cartaobeneficio/Cartao.java
@@ -0,0 +1,14 @@
+package jackson.rocha.cartaobeneficio;
+
+public class Cartao {
+
+    public static final double TARIFA_DEPOSITO = 0.10;
+    public static final double VALOR_MINIMI_DEPOSITO = 50;
+
+    public String titular;
+    public double saldo;
+
+    public Cartao(String titular) {
+        this.titular = titular;
+    }
+}
---
+++ src/main/java/jackson/rocha/cartaobeneficio/Estabelecimento.java
@@ -0,0 +1,11 @@
+package jackson.rocha.cartaobeneficio;
+
+public class Estabelecimento {
+
+    public String nome;
+    public String saldo;
+
+    public Estabelecimento(String nome, String saldo) {
+        this.nome = nome;
+    }
+}
