70a6fb58fd8eeab26be4622c40f3f3b9edc23aa4
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-08 10:49:16 -0300

    Classe hora

---
+++ src/main/java/jackson/rocha/projetoagenda/agenda/Horario.java
@@ -0,0 +1,22 @@
+package jackson.rocha.projetoagenda.agenda;
+
+public class Horario {
+
+    public int[] valores = new int[2];
+
+//    public final int hora;
+//    public final int minuto;
+
+    public Horario(int hora, int minuto) {
+        if (hora < 0 || hora > 23) {
+            throw new IllegalArgumentException("Hora inválida" + hora);
+        }
+
+        if (minuto < 0 || minuto > 59) {
+            throw new IllegalArgumentException("Minuto inválido" + minuto);
+        }
+
+        this.valores[0] = hora;
+        this.valores[1] = minuto;
+    }
+}
