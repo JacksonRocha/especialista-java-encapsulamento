bee2761b2b34d963d0c690cd3fed078cabbde1bd
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-08 10:49:29 -0300

    Chamado a classe hora

---
+++ src/main/java/jackson/rocha/projetoagenda/agenda/Principal.java
@@ -0,0 +1,8 @@
+package jackson.rocha.projetoagenda.agenda;
+
+public class Principal {
+    public static void main(String[] args) {
+        Horario horario = new Horario(10, 55);
+        System.out.printf("%dh%dm", horario.hora, horario.minuto);
+    }
+}
